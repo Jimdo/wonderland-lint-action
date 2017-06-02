6969b5ca65c5836b81b795b8c2976ffd6c528953
Dennis Benkert <dennis.benkert@jimdo.com>
2017-06-02 10:14:50 +0200

    Add more test cases to validate the nomad struct conversion

--- cron/store_nomad_test.go
+++ cron/store_nomad_test.go
@@ -53,7 +53,30 @@ func TestConverStructJob(t *testing.T) {
 		t.Fatalf("Expected nomad job conversion to be successful, but got error: %s", err)
 	}
 
+	// The following tests take some sample fields to validate if the
+	// conversion was correct rather than comparing full instances.
+
+	if j.Name != *apiJob.Name {
+		t.Fatalf("Conversion failed. Expected job name to be %s, but got %s", j.Name, *apiJob.Name)
+	}
+
+	if j.TaskGroups[0].Tasks[0].Config["image"] != apiJob.TaskGroups[0].Tasks[0].Config["image"] {
+		t.Fatalf("Conversion failed. Expected task Docker image to be %s, but got %s", j.TaskGroups[0].Tasks[0].Config["image"], apiJob.TaskGroups[0].Tasks[0].Config["image"])
+	}
+
+	if j.TaskGroups[0].Tasks[0].Env["WONDERLAND_GITHUB_TOKEN"] != apiJob.TaskGroups[0].Tasks[0].Env["WONDERLAND_GITHUB_TOKEN"] {
+		t.Fatalf("Conversion failed. Expected task's environment variable for Wonderland Github token to be %s, but got %s", j.TaskGroups[0].Tasks[0].Env["WONDERLAND_GITHUB_TOKEN"], apiJob.TaskGroups[0].Tasks[0].Env["WONDERLAND_GITHUB_TOKEN"])
+	}
+
 	if j.TaskGroups[0].RestartPolicy.Attempts != *apiJob.TaskGroups[0].RestartPolicy.Attempts {
 		t.Fatalf("Conversion failed. Expected RestartPolicy attempts to be %d, but got %d", j.TaskGroups[0].RestartPolicy.Attempts, *apiJob.TaskGroups[0].RestartPolicy.Attempts)
+	}
+
+	if j.Periodic.ProhibitOverlap != *apiJob.Periodic.ProhibitOverlap {
+		t.Fatalf("Conversion failed. Expected job's periodic overlap configuration to be %t, but got %t", j.Periodic.ProhibitOverlap, *apiJob.Periodic.ProhibitOverlap)
+	}
+
+	if j.Periodic.Enabled != *apiJob.Periodic.Enabled {
+		t.Fatalf("Conversion failed. Expected job's periodic enabled configuration to be %t, but got %t", j.Periodic.Enabled, *apiJob.Periodic.Enabled)
 	}
 }
