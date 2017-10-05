ae81699467e94eab657a8e137c0164a7bf625e16
Daniel Bonkowski <bonko@jimdo.com>
2017-10-05 11:28:04 +0200

    add timeout sidecar container

--- aws/ecs.go
+++ aws/ecs.go
@@ -29,10 +29,27 @@ func NewECSTaskDefinitionStore(e ecsiface.ECSAPI, tdm *ECSTaskDefinitionMapper) 
 }
 
 func (tds *ECSTaskDefinitionStore) AddRevisionFromCronDescription(cronName, family string, desc *cron.CronDescription) (string, error) {
-	out, err := tds.ecs.RegisterTaskDefinition(&ecs.RegisterTaskDefinitionInput{
+
+	rtdInput := &ecs.RegisterTaskDefinitionInput{
 		ContainerDefinitions: []*ecs.ContainerDefinition{tds.tdm.ContainerDefinitionFromCronDescription(family, desc, cronName)},
 		Family:               awssdk.String(family),
-	})
+	}
+
+	// add timeout sidecar container
+	sidecarCapacity := &cron.CapacityDescription{}
+	timeoutSidecarDefinition := &ecs.ContainerDefinition{
+		Command: awssdk.StringSlice([]string{"/bin/sh", "-c", "sleep 10; exit 201"}),
+		Cpu:     awssdk.Int64(int64(sidecarCapacity.CPULimit())),
+		DockerLabels: map[string]*string{
+			"com.jimdo.wonderland.cron": awssdk.String(cronName),
+		},
+		Image:  awssdk.String("alpine:3.6"),
+		Memory: awssdk.Int64(int64(sidecarCapacity.MemoryLimit())),
+		Name:   awssdk.String("timeout"),
+	}
+
+	rtdInput.ContainerDefinitions = append(rtdInput.ContainerDefinitions, timeoutSidecarDefinition)
+	out, err := tds.ecs.RegisterTaskDefinition(rtdInput)
 	if err != nil {
 		return "", fmt.Errorf("could not register task definition for family %q with error: %s", family, err)
 	}
