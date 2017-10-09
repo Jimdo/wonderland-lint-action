df46fbced2971409f35ac5951bb9bcb1b3d0aa69
denderello <spinecrasher@googlemail.com>
2017-10-09 15:53:12 +0200

    Move mocks to their own top level package

--- events/worker_test.go
+++ events/worker_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/golang/mock/gomock"
 
-	"github.com/Jimdo/wonderland-crons/aws/mock"
+	"github.com/Jimdo/wonderland-crons/mock"
 )
 
 var (
