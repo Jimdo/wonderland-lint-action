d75f8ed6f39cc485ecfa7dab477f27e72c442ba5
denderello <spinecrasher@googlemail.com>
2017-12-14 11:08:17 +0100

    Return correct error type for notifications validation

--- validation/cron_notification.go
+++ validation/cron_notification.go
@@ -1,8 +1,6 @@
 package validation
 
 import (
-	"fmt"
-
 	"github.com/Jimdo/wonderland-crons/cron"
 )
 
@@ -13,15 +11,15 @@ func (cn cronNotification) validate(notification *cron.CronNotification) error {
 	ranLongerThanThreshhold := notification.RanLongerThanThreshhold
 
 	if noRunThreshhold == nil && ranLongerThanThreshhold == nil {
-		return fmt.Errorf("At least no-run-threshhold or ran-longer-than-threshhold has to be configured when using notifications")
+		return Error{"At least no-run-threshhold or ran-longer-than-threshhold has to be configured when using notifications"}
 	}
 
 	if noRunThreshhold != nil && *noRunThreshhold < 1 {
-		return fmt.Errorf("The value of no-run-threshhold has to be greater than 0")
+		return Error{"The value of no-run-threshhold has to be greater than 0"}
 	}
 
 	if ranLongerThanThreshhold != nil && *ranLongerThanThreshhold < 1 {
-		return fmt.Errorf("The value of ran-longer-than-threshhold has to be greater than 0")
+		return Error{"The value of ran-longer-than-threshhold has to be greater than 0"}
 	}
 	return nil
 }
