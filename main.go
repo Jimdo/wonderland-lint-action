79903ea31cb8e94f1a87788eb2e381eee2ef1a67
denderello <spinecrasher@googlemail.com>
2018-01-18 15:33:56 +0100

    Remove API v1 and related nomad implementations

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
 	"net/http"
 	"net/http/pprof"
-	"net/url"
 	"os"
 	"time"
 
@@ -18,8 +16,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/gorilla/mux"
-	cleanhttp "github.com/hashicorp/go-cleanhttp"
-	"github.com/hashicorp/nomad/api"
 	log "github.com/sirupsen/logrus"
 	graceful "gopkg.in/tylerb/graceful.v1"
 
@@ -30,14 +26,12 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/Jimdo/wonderland-crons/api/v1"
 	"github.com/Jimdo/wonderland-crons/api/v2"
 	"github.com/Jimdo/wonderland-crons/aws"
 	"github.com/Jimdo/wonderland-crons/cron"
 	"github.com/Jimdo/wonderland-crons/cronitor"
 	"github.com/Jimdo/wonderland-crons/events"
 	"github.com/Jimdo/wonderland-crons/locking"
-	"github.com/Jimdo/wonderland-crons/nomad"
 	"github.com/Jimdo/wonderland-crons/notifications"
 	"github.com/Jimdo/wonderland-crons/store"
 	"github.com/Jimdo/wonderland-crons/validation"
@@ -51,20 +45,9 @@ var (
 		// Server
 		Addr            string        `flag:"addr" default:":8000" description:"The address/port combination to listen on"`
 		ShutdownTimeout time.Duration `flag:"shutdown-timeout" default:"10s" description:"The time to wait for active requests to finish when shutting down"`
-
-		// Nomad
-		NomadURI           string `flag:"nomad-api" env:"NOMAD_API" default:"" description:"The address of nomad API"`
-		NomadUser          string `flag:"nomad-user" env:"NOMAD_USER" default:"" description:"The username to use for the nomad API"`
-		NomadPass          string `flag:"nomad-pass" env:"NOMAD_PASS" default:"" description:"The password to use for the nomad API"`
-		NomadCronPrefix    string `flag:"nomad-cron-prefix" env:"NOMAD_CRON_PREFIX" default:"wlc-" description:"The prefix to use for cron jobs in nomad"`
-		NomadWLDockerImage string `flag:"nomad-wl-docker-image" env:"NOMAD_WL_DOCKER_IMAGE" default:"" description:"The Docker image to use for running wl commands in Nomad"`
-
 		// Vault
 		VaultAddress string `flag:"vault-address" env:"VAULT_ADDR" default:"http://127.0.0.1:8282" description:"Vault Address"`
 		VaultRoleID  string `flag:"vault-role-id" env:"VAULT_ROLE_ID" description:"RoleID with sufficient access"`
-
-		// wl authentication
-		WonderlandGitHubToken string `flag:"wonderland-github-token" env:"WONDERLAND_GITHUB_TOKEN" default:"" description:"The GitHub token to use for wl authentication"`
 
 		// Docker registries
 		WonderlandRegistryAddress string `flag:"wonderland-registry-address" env:"WONDERLAND_REGISTRY_ADDRESS" description:"The address of the Wonderland registry"`
@@ -75,10 +58,11 @@ var (
 		QuayRegistryPass          string `flag:"query-registry-pass" env:"QUAY_REGISTRY_PASS" description:"The passwordfor the Quay registry"`
 
 		// AWS
+
+		// TODO: Check why the region is not used
 		AWSRegion                       string        `flag:"region" env:"AWS_REGION" default:"eu-west-1" description:"The AWS region to use"`
 		CronRoleARN                     string        `flag:"cron-role-arn" env:"CRON_ROLE_ARN" description:"The IAM Role that grants Cloudwatch access to ECS"`
 		ECSClusterARN                   string        `flag:"cluster-arn" env:"ECS_CLUSTER_ARN" description:"The ARN of the ECS cluster crons should run on"`
-		RefreshAWSCredentialsInterval   time.Duration `flag:"aws-credentials-interval" env:"AWS_CREDENTIALS_INTERVAL" default:"10m" description:"Interval in which to fetch new AWS credentials from Vault"`
 		ECSEventsQueueURL               string        `flag:"ecs-events-queue-url" env:"ECS_EVENTS_QUEUE_URL" description:"The URL of the SQS queue receiving ECS events"`
 		ECSEventQueuePollInterval       time.Duration `flag:"ecs-events-queue-poll-interval" default:"100ms" description:"The interval in which to poll new ECS events"`
 		CronsTableName                  string        `flag:"crons-table-name" env:"CRONS_TABLE_NAME" description:"Name of the DynamoDB Table used for storing crons"`
@@ -148,35 +132,6 @@ func main() {
 
 	router := mux.NewRouter()
 
-	nomadURI, err := url.Parse(config.NomadURI)
-	if err != nil {
-		abort("Could not parse Nomad URI: %s", err)
-	}
-
-	clientConfig := &api.Config{
-		Address:    config.NomadURI,
-		HttpClient: cleanhttp.DefaultPooledClient(),
-		HttpAuth: &api.HttpBasicAuth{
-			Username: config.NomadUser,
-			Password: config.NomadPass,
-		},
-		TLSConfig: &api.TLSConfig{
-			TLSServerName: nomadURI.Hostname(),
-		},
-	}
-
-	// This is required for the client to work with a custom HttpClient
-	transport := clientConfig.HttpClient.Transport.(*http.Transport)
-	transport.TLSHandshakeTimeout = 10 * time.Second
-	transport.TLSClientConfig = &tls.Config{
-		MinVersion: tls.VersionTLS12,
-	}
-
-	nomadClient, err := api.NewClient(clientConfig)
-	if err != nil {
-		abort("Could not create Nomad client: %s", err)
-	}
-
 	ecsClient := ecsClient()
 	cwClient := cloudwatchEventsClient()
 	dynamoDBClient := dynamoDBClient()
@@ -236,21 +191,6 @@ func main() {
 			log.Fatalf("RCM library returned error even though ignore errors option was set: %s", err)
 		}
 	}()
-
-	v1.New(&v1.Config{
-		Service: &nomad.CronService{
-			Store: nomad.NewCronStore(&nomad.CronStoreConfig{
-				CronPrefix:    config.NomadCronPrefix,
-				Datacenters:   []string{os.Getenv("AWS_REGION")},
-				Client:        nomadClient,
-				WLDockerImage: config.NomadWLDockerImage,
-				WLEnvironment: os.Getenv("WONDERLAND_ENV"),
-				WLGitHubToken: config.WonderlandGitHubToken,
-			}),
-			Validator: validator,
-		},
-		Router: router.PathPrefix("/v1").Subrouter(),
-	}).Register()
 
 	dynamoDBExecutionStore, err := store.NewDynamoDBExecutionStore(dynamoDBClient, config.ExecutionsTableName)
 	if err != nil {
