187e1dd4c2e6a605f3e96b4b7e512c0dfb033ecd
Mitja Adebahr <extern.mitja.adebahr@jimdo.com>
2020-09-11 08:08:16 +0200

    WL-274 Fix Logzio URL scheme
    Logz.io changed there url scheme for filtering. This broke our current
    template used by the wonderland cli.

--- api/v2/uris.go
+++ api/v2/uris.go
@@ -8,5 +8,5 @@ type URIGenerator struct {
 }
 
 func (u *URIGenerator) CronLogsHTML(cron string) string {
-	return fmt.Sprintf("%s/#/dashboard/kibana?kibanaRoute=%%2Fdiscover%%3F_g%%3D()%%26_a%%3D(columns%%3A!(message)%%2Cindex%%3A%%255BlogzioCustomerIndex%%255DYYMMDD%%2Cinterval%%3Aauto%%2Cquery%%3A(query_string%%3A(analyze_wildcard%%3A!t%%2Cquery%%3A%%2527wonderland.cron%%3A%s%%2527))%%2Csort%%3A!(%%2527%%40timestamp%%2527%%2Cdesc))&switchToAccountId=%s", u.LogzioURL, cron, u.LogzioAccountID)
+	return fmt.Sprintf("%s/#/dashboard/kibana/discover?_a=(columns%%3A!(message)%%2Cindex%%3A'logzioCustomerIndex*'%%2Cinterval%%3Aauto%%2Cquery%%3A(language%%3Alucene%%2Cquery%%3A'wonderland.cron%%3A%s')%%2Csort%%3A!('%%40timestamp'%%2Cdesc))&_g=()&switchToAccountId=%s", u.LogzioURL, cron, u.LogzioAccountID)
 }
